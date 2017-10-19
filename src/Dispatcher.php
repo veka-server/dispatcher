076351f8d8c5845c92947578c3fdf51350a2466a
Nicolas VANHAEZEBROUCK <nvanhaezebrouck@CASTELIS.COM>
2017-10-19 16:29:26 +0200

    ajouter la fonction de send directement dans la classe

--- src/Dispatcher.php
+++ src/Dispatcher.php
@@ -93,4 +93,37 @@ class Dispatcher implements RequestHandlerInterface {
         return $this->process($request);
     }
 
+
+    /**
+     * Send an HTTP response
+     *
+     * @return void
+     */
+    function send(ResponseInterface $response)
+    {
+        $http_line = sprintf('HTTP/%s %s %s',
+            $response->getProtocolVersion(),
+            $response->getStatusCode(),
+            $response->getReasonPhrase()
+        );
+
+        header($http_line, true, $response->getStatusCode());
+
+        foreach ($response->getHeaders() as $name => $values) {
+            foreach ($values as $value) {
+                header("$name: $value", false);
+            }
+        }
+
+        $stream = $response->getBody();
+
+        if ($stream->isSeekable()) {
+            $stream->rewind();
+        }
+
+        while (!$stream->eof()) {
+            echo $stream->read(1024 * 8);
+        }
+    }
+
 }
